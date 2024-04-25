503037c48b8d38cf122c480eb7eb4ea4a84a12c6
mcmuffinoven <ma.anthony1125@gmail.com>
2024-04-25 23:29:31 +0800

    Move insert_product function as an async operation prevent error with blocking functions from discord heartbeats

--- src/commands/price_tracker.py
+++ src/commands/price_tracker.py
@@ -8,6 +8,10 @@ import logging
 from table2ascii import table2ascii as t2a, PresetStyle
 from utils.user import User
 
+import typing
+import functools
+import asyncio
+
 
 log = logging.getLogger(__name__)
 
@@ -15,16 +19,30 @@ class Price_tracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = Postgres(filename="utils/db_info.ini", section="postgres")
-                
+    
+    def to_thread(func: typing.Callable) -> typing.Coroutine:
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            return await asyncio.to_thread(func, *args, **kwargs)
+        return wrapper
+
+
+    @to_thread
+    def scrape_product(self, product_category, product_link:str, user_id):
+        data = self.db.insert_product(product_category=product_category, product_link=product_link, user_id=user_id)
+        log.info(data)
+        
+        return data
+
+
     @commands.command(name="track_product")
     async def track_product(self, ctx: CustomContext, product_category:str=None, product_link:str=None):
         if not product_category:
             await ctx.send("You forgot the product category")
         else:
             # print(Product_category.__members__)
-            print(f"{", ".join(list(Product_category.__members__.keys()))}")
-            # if product_category.upper() not in Product_category.__members__:
-                # await ctx.send(f"Please choose a category from: {", ".join(list(Product_category.__members__.keys()))}")
+            if product_category.upper() not in list(Product_category.__members__.keys()):
+                await ctx.send(f"Please choose a category from: {', '.join(list(Product_category.__members__.keys()))}")
         
         if not product_link:
             await ctx.send("You forgot the product link")
@@ -32,16 +50,16 @@ class Price_tracker(commands.Cog):
         # product = Product(product_category=product_category, product_link=product_link)
         # log.info(ctx.message.author)
         
-        # try:
-        #     data = self.db.insert_product(product_category=product_category, product_link=product_link.upper())
-        #     log.info(data)
+        try:
             
-        #     await ctx.send(f"Adding {data.product_name} with starting price {data.product_current_price} to the database. You will recieve a message when this item is on sale." )
-        #     await ctx.send("Adding")
+            data = await self.scrape_product(product_category,product_link, ctx.message.author)
+            
+            await ctx.send(f"Adding {data.product_name} with starting price {data.product_current_price} to the database. You will recieve a message when this item is on sale." )
+            await ctx.send("Adding")
     
-        # except Exception as e:
-        #     log.error(e)
-        #     # await ctx.send(f"Error adding {product.product_name}" )
+        except Exception as e:
+            log.error(e)
+            # await ctx.send(f"Error adding {product.product_name}" )
             
     @commands.command(name="get_product")
     async def get_all(self,ctx: CustomContext):
